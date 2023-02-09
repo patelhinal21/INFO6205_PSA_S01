b58430f5c8c5e5fcd0b0d8e7c64e340b31bdae78
Hinal Prabodh Patel <patel.hina@northeastern.edu>
2023-02-09 18:16:49 -0500

    "Code for UFClient"

---
+++ src/main/java/edu/neu/coe/info6205/union_find/UFClient.java
@@ -0,0 +1,36 @@
+package edu.neu.coe.info6205.union_find;
+
+import java.util.Random;
+import edu.neu.coe.info6205.union_find.UF_HWQUPC;
+
+public class UFClient {
+
+    private int n;
+
+
+
+
+    public static int count(int n) {
+        UF_HWQUPC uf = new UF_HWQUPC(n);
+        Random random = new Random();
+        int connections = 0;
+        while (uf.components() > 1) {
+            int p = random.nextInt(n);
+            int q = random.nextInt(n);
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+
+            }
+            connections++;
+
+        }
+        return connections;
+    }
+
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int m = count(n);
+        System.out.println("number of objects (n) = " + n + " and number of pairs (m) = " + m);
+    }
+}
